5761c5290b3bffd59106f4e75312fcd811398079
user-ia <service.informatique@chicmt.fr>
2026-08-31 13:44:18 +0200

    chore: advance validated image tag to 4522258

--- install.ps1
+++ install.ps1
@@ -65,8 +65,8 @@ if (-not (Test-Path ".env")) {
     $dbAuto     = (New-Secret).Substring(0, 24)
     Write-Host "  Secrets JWT generes automatiquement." -ForegroundColor DarkGray
 
-    $r = Read-Host "  Tag image GESTOCK [276d9fe]"
-    $imageTag = if ($r -eq "") { "276d9fe" } else { $r }
+    $r = Read-Host "  Tag image GESTOCK [4522258]"
+    $imageTag = if ($r -eq "") { "4522258" } else { $r }
 
     $r = Read-Host "  Mot de passe base de donnees [$dbAuto]"
     $dbPwd = if ($r -eq "") { $dbAuto } else { $r }
