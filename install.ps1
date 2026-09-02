efc053dfb2b0c130eeacde8b64352f6c9c7aa42e
user-ia <service.informatique@chicmt.fr>
2026-09-02 11:42:26 +0200

    chore: validate release 5c0720a

--- install.ps1
+++ install.ps1
@@ -65,8 +65,8 @@ if (-not (Test-Path ".env")) {
     $dbAuto     = (New-Secret).Substring(0, 24)
     Write-Host "  Secrets JWT generes automatiquement." -ForegroundColor DarkGray
 
-    $r = Read-Host "  Tag image GESTOCK [906c576]"
-    $imageTag = if ($r -eq "") { "906c576" } else { $r }
+    $r = Read-Host "  Tag image GESTOCK [5c0720a]"
+    $imageTag = if ($r -eq "") { "5c0720a" } else { $r }
 
     $r = Read-Host "  Mot de passe base de donnees [$dbAuto]"
     $dbPwd = if ($r -eq "") { $dbAuto } else { $r }
