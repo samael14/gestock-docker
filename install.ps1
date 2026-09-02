22f5850f3c223f7e0c9226e63f90cdae89bb3f14
user-ia <service.informatique@chicmt.fr>
2026-09-02 12:41:14 +0200

    chore: pin deployment to fec26bc

--- install.ps1
+++ install.ps1
@@ -65,8 +65,8 @@ if (-not (Test-Path ".env")) {
     $dbAuto     = (New-Secret).Substring(0, 24)
     Write-Host "  Secrets JWT generes automatiquement." -ForegroundColor DarkGray
 
-    $r = Read-Host "  Tag image GESTOCK [30f09dd]"
-    $imageTag = if ($r -eq "") { "30f09dd" } else { $r }
+    $r = Read-Host "  Tag image GESTOCK [fec26bc]"
+    $imageTag = if ($r -eq "") { "fec26bc" } else { $r }
 
     $r = Read-Host "  Mot de passe base de donnees [$dbAuto]"
     $dbPwd = if ($r -eq "") { $dbAuto } else { $r }
