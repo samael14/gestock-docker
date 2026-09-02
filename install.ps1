bda9caba20b3d1e5dd3cb55ab6cdcd55bec32e0f
user-ia <service.informatique@chicmt.fr>
2026-09-02 16:05:28 +0200

    chore: pin deployment images to 35ff947

--- install.ps1
+++ install.ps1
@@ -65,8 +65,8 @@ if (-not (Test-Path ".env")) {
     $dbAuto     = (New-Secret).Substring(0, 24)
     Write-Host "  Secrets JWT generes automatiquement." -ForegroundColor DarkGray
 
-    $r = Read-Host "  Tag image GESTOCK [86f869d]"
-    $imageTag = if ($r -eq "") { "86f869d" } else { $r }
+    $r = Read-Host "  Tag image GESTOCK [35ff947]"
+    $imageTag = if ($r -eq "") { "35ff947" } else { $r }
 
     $r = Read-Host "  Mot de passe base de donnees [$dbAuto]"
     $dbPwd = if ($r -eq "") { $dbAuto } else { $r }
